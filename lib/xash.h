847800c4eff643fc068f5cb0636a22730a59da32
yohimik <yohimik@gmail.com>
2025-06-17 23:57:54 +0400

    fix: comment

--- lib/xash.h
+++ lib/xash.h
@@ -5,8 +5,6 @@
 extern "C" {
 #endif
 
-// These function signatures must match the actual definitions in the C source.
-// You need to choose the real entry points from the engine code.
 int Launcher_Main( int argc, char **argv );
 
 #ifdef __cplusplus
